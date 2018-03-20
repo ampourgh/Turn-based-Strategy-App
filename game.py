855086d3c86dfe9c0be219d5a18b51b88bacc5f8
ampourgh <ampourgh@gmail.com>
2018-03-19 21:11:02 -0500

    feat: more options for actions with jug

--- game.py
+++ game.py
@@ -1,8 +1,8 @@
 import sys
 
-from character import Character
 from monster import Dragon, Goblin, Troll, Monster
 from clear import clear_screen
+from combat import Combat
 import random
 
 
@@ -98,6 +98,101 @@ class Game:
     elif self.monsters or self.monster:
       print("You lose!")
     sys.exit()
+
+class Character(Combat, Game):
+  attack_limit = 100
+  experience = 0
+  base_hit_points = 10
+
+  def attack(self):
+    roll = random.randint(1, self.attack_limit)
+    if self.weapon == 'sword':
+      roll += 1
+    elif self.weapon == 'axe':
+      roll += 2
+    return roll > 4
+
+  def get_weapon(self):
+
+    weapon_choice = input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
+
+    if weapon_choice in 'sab':
+      if weapon_choice == 's':
+        return 'sword'
+      elif weapon_choice == 'a':
+        return 'axe'
+      else:
+        return 'bow'
+    else:
+      return self.get_weapon()
+
+  def __init__(self, **kwargs):
+    self.name = input("Name: ")
+    self.weapon = self.get_weapon()
+    self.hit_points = self.base_hit_points
+
+    for key, value in kwargs.items():
+      setattr(self, key, value)
+
+  def __str__(self):
+    return '{}, HP: {}, XP: {}'.format(self.name, self.hit_points, self.experience)
+
+  def rest(self):
+    if self.hit_points < self.base_hit_points:
+      self.hit_points += 1
+
+  def leveled_up(self):
+    return self.experience >= 5
+
+  items = ['potion', 'potion', 'bottle of dust']
+
+  def inventory(self):
+
+      self.show_inventory(self.items)
+
+      player_choice = input("\nType in item number: ")
+
+      if ((int(player_choice) - 1) < len(self.items)):
+          if (self.items[(int(player_choice) - 1)] == 'potion'):
+              self.rest()
+              self.rest()
+              print("You feel rejuvenated!")
+              discard_choice = input("")
+              emptyJug = input("\nDiscard the empty jug? [Y]es | [N]o | [O]ther Options ").lower()
+              if emptyJug == 'y':
+                  print("You drop the jug to the ground and it shatters to pieces.")
+                  del self.items[(int(player_choice) - 1):(int(player_choice)):]
+              elif emptyJug == 'o':
+                  options = input("[T]hrow at Monster | [E]at jug | [W]ear as a hat").lower()
+                  del self.items[(int(player_choice) - 1):(int(player_choice)):]
+                  if options == 't':
+                      print("You threw the jug at the monster, causing some damage.")
+                  elif options == 'e':
+                      print("You begin eating the jug, your health points drop.")
+                  elif options == 'w':
+                      print("The sorrounding monsters give you a puzzled stare.")
+                  else:
+                      print("You did nothing with the bottle.")
+              else:
+                  print("You tucked the jug back into your inventory.")
+                  del self.items[(int(player_choice) - 1):(int(player_choice)):]
+                  self.items.append('empty jug')
+
+          else:
+              print("The item you picked out did nothing and vanished!")
+              del self.items[(int(player_choice) - 1):(int(player_choice)):]
+      else:
+          print("Your inventory does not go that deep, it seems...")
+
+  def show_inventory(self, items):
+    try:
+      counter = 1
+      print("\n")
+      for i in items:
+        print("[" + str(counter) + "] " + i)
+        counter += 1
+    except IndexError:
+      return None
 
 
 Game()
